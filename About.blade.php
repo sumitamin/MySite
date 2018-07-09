769fddbde3a07636717d6dfea094c3c6483ce65f
sumit amin <sumitamin04@gmail.com>
2018-07-09 13:26:56 +0530

    minor on 9 july

--- About.blade.php
+++ About.blade.php
@@ -1,4 +1,13 @@
 @extends('Layout.template')
 @section('content')
-<h1>Welcome to index page</h1>
+<div class='container'>
+    <h1>Welcome to index page</h1>
+    @if(count($about_data) > 0)
+        <ul class='list-group'>
+        @foreach($about_data as $hdata)
+            <li class='list-group-item' >{{$hdata}}</li>
+        @endforeach
+        </ul>
+    @endif
+</div>
 @endsection
